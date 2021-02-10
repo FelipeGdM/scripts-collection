e516b097774208a5a3547032ea9d67c1993d710c
Felipe Gomes de Melo <felipegmelo.42@gmail.com>
2021-02-10 20:20:47 -0300

    Fix fisher instalation

--- fresh_start.sh
+++ fresh_start.sh
@@ -6,9 +6,10 @@ sudo apt-add-repository ppa:fish-shell/release-3
 sudo apt-get update
 sudo apt-get install fish -y
 
-curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisherfisher install jorgebucaran/nvm.fish
-fisher install edc/bass
-fisher install jorgebucaran/nvm.fish
+fish -c "curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher"
+fish -c "fisher install jorgebucaran/nvm.fish"
+fish -c "fisher install edc/bass"
+fish -c "fisher install jorgebucaran/nvm.fish"
 
 curl -fsSL https://starship.rs/install.sh | bash
 
