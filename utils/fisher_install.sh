73f5366d6798e09a90ab25ae52b02f0abc587844
Felipe Gomes de Melo <felipegmelo.42@gmail.com>
2021-02-10 20:27:03 -0300

    Separate fish-sprcific commands

---
+++ utils/fisher_install.sh
@@ -0,0 +1,6 @@
+#!/bin/fish
+
+curl -sL https://git.io/fisher | source && fisher install jorgebucaran/fisher
+fisher install jorgebucaran/nvm.fish
+fisher install edc/bass
+fisher install jorgebucaran/nvm.fish
