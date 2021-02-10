961655970c8888e938cff508a4546cb691a7d944
Felipe Gomes de Melo <felipegmelo.42@gmail.com>
2021-02-10 18:35:56 -0300

    Create dir before create file

--- fresh_start.sh
+++ fresh_start.sh
@@ -10,6 +10,7 @@ fisher install jorgebucaran/nvm.fish
 
 curl -fsSL https://starship.rs/install.sh | bash
 
+mkdir -p ~/.config/fish/
 echo "starship init fish | source" > ~/.config/fish/config.fish
 
 sudo chsh -s $(which fish)
